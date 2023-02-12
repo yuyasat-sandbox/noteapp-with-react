aa89335ca4addedc6dda9d38a00da7bfd18cb144
yuyasat <takiyour@gmail.com>
2023-02-12 21:48:24 +0900

    prepare components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <div className="App">
+      <Sidebar />
+      <Main />
     </div>
   )
 }
