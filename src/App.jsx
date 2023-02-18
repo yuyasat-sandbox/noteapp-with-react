2fcc0614f39f64c729c0c6b57bce6f6e125644f4
yuyasat <takiyour@gmail.com>
2023-02-18 22:25:54 +0900

    implement notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import './App.css'
 import Main from './components/Main'
 import Sidebar from './components/Sidebar'
+import uuid from "react-uuid"
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log("新しくノートが追加されました")
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  }
 
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import './Sidebar.css';
 
-const Sidebar = () => {
+const Sidebar = ({ onAddNote, notes }) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>ノート</h1>
-        <button>追加</button>
+        <button onClick={onAddNote}>追加</button>
       </div>
       <div className="app-sidebar-notes">
-        <div className="app-sidebar-note">
-          <div className="sidebar-note-title">
-            <strong>タイトル</strong>
-            <button>削除</button>
+        {notes.map((note) => (
+          <div className="app-sidebar-note" key={note.id}>
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button>削除</button>
+            </div>
+            <p>{note.content}</p>
+            <small>{new Date(note.modDate).toLocaleDateString("ja-JP", {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}</small>
           </div>
-          <p>ノートの内容です。</p>
-          <small>最後の修正日:xx/xx</small>
-        </div>
+        ))}
       </div>
     </div>
   )
