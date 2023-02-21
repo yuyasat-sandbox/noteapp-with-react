8e995495f831892d4c651651f8c107bc8c8ca08a
yuyasat <takiyour@gmail.com>
2023-02-21 21:43:03 +0900

    edit note

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,18 @@ function App() {
     return notes.find((note) => note.id === activeNote)
   }
 
+  const onUpdateNote = (updatedNote) => {
+    // 修正された新しいノートの配列を返す。
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    })
+    setNotes(updatedNotesArray);
+  };
+
   return (
     <div className="App">
       <Sidebar
@@ -38,7 +50,10 @@ function App() {
         activeNote={activeNote}
         setActiveNote={setActiveNote}
       />
-      <Main activeNote={getActiveNote()} />
+      <Main
+        activeNote={getActiveNote()}
+        onUpdateNote={onUpdateNote}
+      />
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import './Main.css';
 
-const Main = ({ activeNote }) => {
+const Main = ({ activeNote, onUpdateNote }) => {
+  const onEditNote = (key, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [key]: value,
+      modDate: Date.now,
+    });
+  }
   if (!activeNote) {
     return <div className='no-active-note'>ノートが選択されていません。</div>
   }
@@ -9,8 +16,18 @@ const Main = ({ activeNote }) => {
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
-        <input type="text" />
-        <textarea id="" placeholder='ノート内容を記入'></textarea>
+        <input
+          id="title"
+          type="text"
+          value={activeNote.title}
+          onChange={(e) => onEditNote("title", e.target.value)}
+        />
+        <textarea
+          id="content"
+          placeholder='ノート内容を記入'
+          value={activeNote.content}
+          onChange={(e) => onEditNote("content", e.target.value)}
+        ></textarea>
       </div>
       <div className="app-main-note-preview">
         <h1 className="preview-title">{activeNote.title}</h1>
