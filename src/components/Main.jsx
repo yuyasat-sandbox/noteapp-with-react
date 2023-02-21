1dd7a6b728795c6eaf3a71fc182ce4086d515b32
yuyasat <takiyour@gmail.com>
2023-02-21 21:23:20 +0900

    main css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,16 @@ import './Main.css';
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder='ノート内容を記入'></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">タイトル</h1>
+        <div className="markdown-preview">ノート内容</div>
+      </div>
+    </div>
   )
 }
 
