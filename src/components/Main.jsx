ebdff528002dcaf24ca3582a3bae95e05e65094d
yuyasat <takiyour@gmail.com>
2023-02-21 21:30:47 +0900

    noteをmainに表示

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './Main.css';
 
-const Main = () => {
+const Main = ({ activeNote }) => {
+  if (!activeNote) {
+    return <div className='no-active-note'>ノートが選択されていません。</div>
+  }
+
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
@@ -9,8 +13,8 @@ const Main = () => {
         <textarea id="" placeholder='ノート内容を記入'></textarea>
       </div>
       <div className="app-main-note-preview">
-        <h1 className="preview-title">タイトル</h1>
-        <div className="markdown-preview">ノート内容</div>
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <div className="markdown-preview">{activeNote.content}</div>
       </div>
     </div>
   )
